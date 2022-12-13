349d82e539f8753ffd9c9779f153500956198bc0
nnamdi16 <nwabuokeinnamdi@gmail.com>
2022-12-14 00:05:06 +0100

    chore: database setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://nnamdi16:Js5pEOQ7Jl9PqPtn@cluster0.ohalqgt.mongodb.net/survey-app',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
