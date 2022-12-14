c97a3b8a69c8516e97357e8c66aa2f1680571f7f
nnamdi16 <nwabuokeinnamdi@gmail.com>
2022-12-14 11:55:52 +0100

    chore: added validation to the GlobalPipe

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { AppModule } from './app.module';
 import * as compression from 'compression';
 import helmet from 'helmet';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(compression());
   app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
     .setTitle('Survey App APIs')
     .setDescription('API service to power a Survey Application')
