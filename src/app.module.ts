6d715f5d79f37d24caddad2d9147cf63e5412e4e
nnamdi16 <nwabuokeinnamdi@gmail.com>
2022-12-14 11:55:23 +0100

    chore: added UserModule and SurveyModule to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SurveyModule } from './survey/survey.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb+srv://nnamdi16:Js5pEOQ7Jl9PqPtn@cluster0.ohalqgt.mongodb.net/survey-app',
     ),
+    SurveyModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
