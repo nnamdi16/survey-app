82d5c106e49b47296fc0398a307c9e2dadce3488
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-01-03 09:22:37 +0100

    chore: refactored the configuration of database using ConfigModule and ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,35 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SurveyModule } from './survey/survey.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://nnamdi16:Js5pEOQ7Jl9PqPtn@cluster0.ohalqgt.mongodb.net/survey-app',
-    ),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        MONGO_USERNAME: Joi.string().required(),
+        MONGO_PASSWORD: Joi.string().required(),
+        MONGO_DATABASE: Joi.string().required(),
+        MONGO_HOST: Joi.string().required(),
+      }),
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const username = configService.get('MONGO_USERNAME');
+        const password = configService.get('MONGO_PASSWORD');
+        const database = configService.get('MONGO_DATABASE');
+        const host = configService.get('MONGO_HOST');
+        const uri = `mongodb+srv://${username}:${password}@${host}`;
+
+        return {
+          uri,
+          dbName: database,
+        };
+      },
+      inject: [ConfigService],
+    }),
     SurveyModule,
     UsersModule,
   ],
