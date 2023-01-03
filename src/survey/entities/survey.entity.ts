13bc306eb01eae914e6e9747da6d05ecebddbfca
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-01-03 09:32:58 +0100

    chore: refactored the Survey entity

--- src/survey/entities/survey.entity.ts
+++ src/survey/entities/survey.entity.ts
@@ -1,13 +1,20 @@
 import { Prop, Schema } from '@nestjs/mongoose';
-import { Types } from 'mongoose';
+import { HydratedDocument, Types } from 'mongoose';
 import { User } from 'src/users/entities/user.entity';
 import { Question, QuestionSchema } from './question.entity';
 
+export type SurveyDocument = HydratedDocument<Survey>;
 @Schema({ timestamps: true, collection: 'survey' })
-export class Survey extends Document {
-  @Prop({ type: [QuestionSchema], required: true })
-  questions: Question[];
+export class Survey {
+  @Prop({ type: [QuestionSchema], required: true, default: () => [] })
+  questions: [Question];
 
   @Prop({ type: Types.ObjectId, ref: 'User', required: true, index: true })
   userId: User;
+
+  @Prop({ required: true, max: 450 })
+  title: string;
+
+  @Prop({ required: true, max: 32000 })
+  description: string;
 }
