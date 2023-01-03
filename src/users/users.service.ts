2244cd44ee6ccd4cc3164e41aa9cfba9242922bd
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-01-03 09:39:25 +0100

    chore: Implemented the create method and the findOne method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,19 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User, UserDocument } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+  ) {}
+  async create(
+    createUserDto: CreateUserDto,
+  ): Promise<Omit<UserDocument, 'hash' | 'salt'>> {
+    const { email } = createUserDto;
+    const existingUser = await this.findOne({ email });
+    console.log(existingUser);
+    if (existingUser) {
+      throw new BadRequestException({
+        status: HttpStatus.BAD_REQUEST,
+        message: 'User with email or mobile already exists',
+      });
+    }
+    const newUser = new this.userModel(createUserDto);
+    newUser.encryptPassword(createUserDto.password);
+    newUser.save();
+    const { salt, hash, ...userDetails } = newUser;
+    return userDetails;
   }
 
   findAll() {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(
+    profile: Partial<Pick<UserDocument, '_id' | 'email' | 'mobile'>>,
+  ): Promise<UserDocument> {
+    return await this.userModel.findOne(profile).exec();
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
