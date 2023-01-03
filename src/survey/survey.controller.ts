e85f4904387e9bef857d5cb0c6a95f41b3767ca7
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-01-03 09:23:50 +0100

    chore: added survey tag in the survey controller

--- src/survey/survey.controller.ts
+++ src/survey/survey.controller.ts
@@ -10,7 +10,9 @@ import {
 import { SurveyService } from './survey.service';
 import { CreateSurveyDto } from './dto/create-survey.dto';
 import { UpdateSurveyDto } from './dto/update-survey.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Survey')
 @Controller('survey')
 export class SurveyController {
   constructor(private readonly surveyService: SurveyService) {}
