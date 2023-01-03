fb19fe9f8bedf9b4b4b26029d21318b82c0b620d
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-01-03 09:32:38 +0100

    chore: refactored the question entity

--- src/survey/entities/question.entity.ts
+++ src/survey/entities/question.entity.ts
@@ -1,19 +1,24 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { IResponseType } from '../enum/responseType.enum';
+import { HydratedDocument } from 'mongoose';
+import { IQuestionType } from '../enum/responseType.enum';
 
+export type QuestionDocument = HydratedDocument<Question>;
 @Schema({ timestamps: true })
-export class Question extends Document {
-  @Prop({ required: true })
+export class Question {
+  @Prop({ required: true, max: 450 })
   title: string;
 
-  @Prop({ required: true, type: IResponseType })
-  responseType: IResponseType;
+  @Prop({ required: true, max: 32000 })
+  description: string;
+
+  @Prop({ required: true, type: IQuestionType })
+  questionType: IQuestionType;
 
   @Prop({ required: true })
   required: boolean;
 
   @Prop({ required: false, default: null })
-  responseTypeData: string;
+  questionTypeData: string;
 }
 
 export const QuestionSchema = SchemaFactory.createForClass(Question);
