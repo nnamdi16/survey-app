f0d6c8d2dc199618e30dcd051e4207ab3ad7b47d
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-01-06 21:40:02 +0100

    chore:refactored create method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,5 @@
 import { BadRequestException, HttpStatus, Injectable } from '@nestjs/common';
+import { UtilHelpers } from 'src/util/util';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserDocument } from './entities/user.entity';
@@ -10,19 +11,18 @@ export class UsersService {
   async create(
     createUserDto: CreateUserDto,
   ): Promise<Omit<UserDocument, 'hash' | 'salt'>> {
-    const { email } = createUserDto;
+    const { email, password, ...otherParams } = createUserDto;
     const existingUser = await this.userRepository.findOne({ email });
-    console.log(existingUser);
     if (existingUser) {
       throw new BadRequestException({
         status: HttpStatus.BAD_REQUEST,
         message: 'User with email or mobile already exists',
       });
     }
-    const { password, credentials, ...otherParams } = createUserDto;
-    const userData = { ...credentials, otherParams };
-    const newUser = await this.userRepository.create(userData);
-    const { salt, hash, ...userDetails } = newUser;
+    const userSecret = UtilHelpers.encryptPassword(password);
+    const userData = { ...userSecret, email, ...otherParams };
+    const { _doc } = await this.userRepository.create(userData);
+    const { salt, hash, __v, ...userDetails } = _doc;
     return userDetails;
   }
 
