fba88a0bc63798dec1224ce0b9dbd93da0a5f83c
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-01-06 15:24:49 +0100

    feat: mocked the model

---
+++ src/database/test/__mocks__/mock.model.ts
@@ -0,0 +1,12 @@
+export abstract class MockModel<T> {
+  protected abstract entityStub: T;
+
+  constructor(createEntityData: T) {
+    this.constructorSpy(createEntityData);
+  }
+  constructorSpy(_createEntityData: T): void {}
+
+  async save(): Promise<T> {
+    return this.entityStub;
+  }
+}
