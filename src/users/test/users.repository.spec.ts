9c47a48cb60dc07ced4285c34ad4dd788d464cee
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-01-07 22:20:16 +0100

    chore: tested the create method, testing the model and the state when the user is created

--- src/users/test/users.repository.spec.ts
+++ src/users/test/users.repository.spec.ts
@@ -2,6 +2,8 @@ import { getModelToken } from '@nestjs/mongoose';
 import { Test } from '@nestjs/testing';
 import { User } from '../entities/user.entity';
 import { UsersRepository } from '../users.repository';
+import { createUserEntityStub, createUserStub } from './stubs/user.stubs';
+import { UserModel } from './support/user.model';
 
 describe('UsersRepository', () => {
   let userRepository: UsersRepository;
@@ -13,15 +15,33 @@ describe('UsersRepository', () => {
           UsersRepository,
           {
             provide: getModelToken(User.name),
-            useValue: UserMode
+            useValue: UserModel,
           },
         ],
-      });
+      }).compile();
+      userRepository = moduleRef.get<UsersRepository>(UsersRepository);
     });
     describe('When create is called', () => {
       let user: User;
       let saveSpy: jest.SpyInstance;
       let constructorSpy: jest.SpyInstance;
+
+      beforeEach(async () => {
+        saveSpy = jest.spyOn(UserModel.prototype, 'save');
+        constructorSpy = jest.spyOn(UserModel.prototype, 'constructorSpy');
+        user = (await userRepository.create(
+          createUserStub(),
+        )) as unknown as User;
+      });
+
+      test('then it should call the userModel', () => {
+        expect(saveSpy).toHaveBeenCalled();
+        expect(constructorSpy).toHaveBeenLastCalledWith(createUserStub());
+      });
+
+      test('then it should return a user', () => {
+        expect(user).toEqual(createUserEntityStub());
+      });
     });
   });
 });
