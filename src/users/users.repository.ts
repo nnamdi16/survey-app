80d804859e1ccd6bf6abe526fd2cb6218d26c769
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-01-07 22:15:50 +0100

    chore: changed the path of import

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,9 +1,8 @@
 import { InjectModel } from '@nestjs/mongoose';
 import { Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
-import { EntityRepository } from 'src/database/entity.repository';
+import { EntityRepository } from '../../src/database/entity.repository';
 import { User, UserDocument } from './entities/user.entity';
-
 @Injectable()
 export class UsersRepository extends EntityRepository<UserDocument> {
   constructor(@InjectModel(User.name) userModel: Model<UserDocument>) {
