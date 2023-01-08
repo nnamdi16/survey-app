9fb82686848e1906af009ccbb2cff410e8069ae0
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-01-08 06:38:00 +0100

    chore: changed file path for entity repository

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,8 +1,9 @@
 import { InjectModel } from '@nestjs/mongoose';
 import { Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
-import { EntityRepository } from '../../src/database/entity.repository';
 import { User, UserDocument } from './entities/user.entity';
+import { EntityRepository } from 'src/database/entity.repository';
+
 @Injectable()
 export class UsersRepository extends EntityRepository<UserDocument> {
   constructor(@InjectModel(User.name) userModel: Model<UserDocument>) {
