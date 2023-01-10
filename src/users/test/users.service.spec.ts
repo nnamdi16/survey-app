5c67537271e399ba17691931c04bfaab3ebc9798
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-01-10 17:46:19 +0100

    chore: removed unused lines of code

--- src/users/test/users.service.spec.ts
+++ src/users/test/users.service.spec.ts
@@ -1,34 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { CreateUserDto } from '../dto/create-user.dto';
 import { UsersService } from '../users.service';
-import { IUser } from '../interface/user.interface';
-import { User } from '../entities/user.entity';
 import { createUserStub, userStub } from './stubs/user.stubs';
 import { UsersRepository } from '../users.repository';
 import { BadRequestException, HttpException, HttpStatus } from '@nestjs/common';
 // https://github.com/mguay22/nestjs-mongo
 
-const mockUser = (user: CreateUserDto): Omit<IUser, 'password'> => {
-  const { firstName, lastName, otherNames, email, mobile } = user;
-  return {
-    firstName,
-    lastName,
-    otherNames,
-    email,
-    mobile,
-  };
-};
-
-const mockUserDoc = (mock?: Partial<IUser>): Partial<User> => ({
-  firstName: mock?.firstName || 'John',
-  lastName: mock?.lastName || 'Doe',
-  // _id: new Types.ObjectId(uuidv4()),
-});
 
 describe('UsersService', () => {
   let service: UsersService;
-  // let repository: UsersRepository;
-  // let model: Model<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
