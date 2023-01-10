368891d24ccbbf08365751968140a46f9b02cb1d
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-01-10 17:41:17 +0100

    chore:added test for the user service

--- src/users/test/users.service.spec.ts
+++ src/users/test/users.service.spec.ts
@@ -3,10 +3,9 @@ import { CreateUserDto } from '../dto/create-user.dto';
 import { UsersService } from '../users.service';
 import { IUser } from '../interface/user.interface';
 import { User } from '../entities/user.entity';
-import { Model } from 'mongoose';
-import { getModelToken } from '@nestjs/mongoose';
-import { createUserStub } from './stubs/user.stubs';
+import { createUserStub, userStub } from './stubs/user.stubs';
 import { UsersRepository } from '../users.repository';
+import { BadRequestException, HttpException, HttpStatus } from '@nestjs/common';
 // https://github.com/mguay22/nestjs-mongo
 
 const mockUser = (user: CreateUserDto): Omit<IUser, 'password'> => {
@@ -28,31 +27,24 @@ const mockUserDoc = (mock?: Partial<IUser>): Partial<User> => ({
 
 describe('UsersService', () => {
   let service: UsersService;
-  let repository: UsersRepository;
-  let model: Model<User>;
+  // let repository: UsersRepository;
+  // let model: Model<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UsersService,
-        UsersRepository,
         {
-          provide: getModelToken(User.name),
+          provide: UsersRepository,
           useValue: {
-            new: jest.fn().mockResolvedValue(mockUser(createUserStub())),
-            constructor: jest
-              .fn()
-              .mockResolvedValue(mockUser(createUserStub())),
-            create: jest.fn(),
-            findOne: jest.fn(),
+            create: jest.fn().mockResolvedValue(userStub()),
+            findOne: jest.fn().mockResolvedValue(userStub()),
           },
         },
       ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
-    repository = module.get<UsersRepository>(UsersRepository);
-    model = module.get<Model<User>>(getModelToken('User'));
   });
 
   it('should be defined', () => {
@@ -64,53 +56,23 @@ describe('UsersService', () => {
   });
 
   describe('create operation', () => {
-    // beforeEach(async () => {
-    //   const moduleRef = await Test.createTestingModule({
-    //     providers: [
-    //       UsersRepository,
-    //       {
-    //         provide: getModelToken(User.name),
-    //         useValue: UserModel,
-    //       },
-    //     ],
-    //   }).compile();
-    //   userRepository = moduleRef.get<UsersRepository>(UsersRepository);
-    // });
     describe('When create is called', () => {
-      let user: User;
-      let createUser: CreateUserDto;
-      // let saveSpy: jest.SpyInstance;
-      // let constructorSpy: jest.SpyInstance;
-
-      beforeEach(async () => {
-        // createSpy = jest.spyOn();
-        // saveSpy = jest.spyOn(UserModel.prototype, 'save');
-        // constructorSpy = jest.spyOn(UserModel.prototype, 'constructorSpy');
-        user = (await service.create(createUserStub())) as unknown as User;
-        // user = (await userRepository.create(
-        //   createUserStub(),
-        // )) as unknown as User;
+      test('then it should call the userModel', async () => {
+        try {
+          await service.create(createUserStub());
+        } catch (error) {
+          expect(error).toBeInstanceOf(HttpException);
+        }
       });
 
-      test('then it should call the userModel', () => {
-        // expect(saveSpy).toHaveBeenCalled();
-        expect(repository.create).toHaveBeenLastCalledWith(createUserStub());
+      test('then it should call the userModel', async () => {
+        await expect(service.create(createUserStub())).rejects.toEqual(
+          new BadRequestException({
+            status: HttpStatus.BAD_REQUEST,
+            message: 'User with email or mobile already exists',
+          }),
+        );
       });
-
-      // test('then it should return a user', () => {
-      //   expect(user).toEqual(createUserEntityStub());
-      // });
     });
   });
-
-  // it('should create a new user', async () => {
-  //   jest.spyOn(model, 'create').mockImplementationOnce(() =>
-  //     Promise.resolve({
-  //       _id: new Types.ObjectId(uuidv4()),
-  //       ...userStub(),
-  //     }),
-  //   );
-  //   const newUser = await service.create(createUserStub());
-  //   expect(newUser).toEqual(mockUser(createUserStub()));
-  // });
 });
