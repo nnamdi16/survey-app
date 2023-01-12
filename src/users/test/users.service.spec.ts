828a1be1fe11a5523f5e32546961f5bd180b13a5
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-01-12 10:31:15 +0100

    chore: refactored the user  service test

--- src/users/test/users.service.spec.ts
+++ src/users/test/users.service.spec.ts
@@ -3,20 +3,22 @@ import { UsersService } from '../users.service';
 import { createUserStub, userStub } from './stubs/user.stubs';
 import { UsersRepository } from '../users.repository';
 import { BadRequestException, HttpException, HttpStatus } from '@nestjs/common';
+import { UtilHelpers } from 'src/util/util';
 // https://github.com/mguay22/nestjs-mongo
-
 
 describe('UsersService', () => {
   let service: UsersService;
+  const utilHelper = jest.fn();
+  let repository: UsersRepository;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UsersService,
         {
           provide: UsersRepository,
           useValue: {
-            create: jest.fn().mockResolvedValue(userStub()),
+            create: jest.fn().mockResolvedValue({ _doc: userStub() }),
             findOne: jest.fn().mockResolvedValue(userStub()),
           },
         },
@@ -24,6 +26,7 @@ describe('UsersService', () => {
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    repository = module.get<UsersRepository>(UsersRepository);
   });
 
   it('should be defined', () => {
@@ -51,6 +54,21 @@ describe('UsersService', () => {
             message: 'User with email or mobile already exists',
           }),
         );
+      });
+
+      test('should encrypt the password', async () => {
+        jest.clearAllMocks();
+        jest.spyOn(repository, 'findOne').mockResolvedValue(null);
+        UtilHelpers.encryptPassword = utilHelper;
+        const expectedValue = {
+          salt: '9460efe9642e5618e7bdc5a5f1a3c63a',
+          hash: '4fd1a475d4fb273ed0e46e2774c1894b8763e6a61943865a602389d83a4201029b223e1bd715bd4aaee0683f4d474769cb35bfcd70e21c493abda0ad841b8e8c',
+        };
+        utilHelper.mockReturnValue(expectedValue);
+
+        const result = await service.create(createUserStub());
+        expect(utilHelper).toHaveBeenCalledWith(createUserStub().password);
+        expect(result).toEqual(userStub());
       });
     });
   });
