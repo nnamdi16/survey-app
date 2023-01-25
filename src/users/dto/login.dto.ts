b0f95130573213ff9134d565a2918191d19de882
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-01-25 23:15:53 +0100

    chore: removed the LoginResponseDto

--- src/users/dto/login.dto.ts
+++ src/users/dto/login.dto.ts
@@ -1,8 +1,6 @@
-import { HttpStatus } from '@nestjs/common';
 import { ApiProperty, PickType } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
 import { IsString } from 'class-validator';
-import { STATUS } from 'src/util/constant';
 import { CreateUserDto } from './create-user.dto';
 
 export class LoginDto extends PickType(CreateUserDto, ['password'] as const) {
@@ -15,30 +13,4 @@ export class LoginDto extends PickType(CreateUserDto, ['password'] as const) {
   })
   @Transform(({ value }) => String(value).toLowerCase().trim())
   email: string;
-}
-
-export class LoginResponse {
-  @ApiProperty({
-    description: 'Response Message',
-    example: STATUS.SUCCESS,
-    required: true,
-    title: 'message',
-  })
-  message: string;
-
-  @ApiProperty({
-    description: 'User token',
-    example: '',
-    required: true,
-    title: 'token',
-  })
-  token: string;
-
-  @ApiProperty({
-    description: 'Response Http status',
-    example: HttpStatus.OK,
-    required: true,
-    title: 'status',
-  })
-  status: HttpStatus;
 }
